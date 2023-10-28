c5c27332b06fda72e2026d88f0d58e27f12bc655
James Harvey <44349936+jmshrv@users.noreply.github.com>
2023-10-28 17:57:48 +0100

    plunder thy cursed flask app

---
+++ backend/src/app.py
@@ -0,0 +1,20 @@
+import datetime
+from opensky_api import OpenSkyApi
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    now = datetime.datetime.now()
+    yesterday = now - datetime.timedelta(1)
+    print(now.timestamp())
+    print(yesterday.timestamp())
+    api = OpenSkyApi(
+        username=os.environ["OPENSKY_USERNAME"], password=os.environ["OPENSKY_PASSWORD"]
+    )
+    s = api.get_states(icao24="3c65d6")
+    print(s)
+    return "succeeded?"
